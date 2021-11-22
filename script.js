4cff8936dfac28412ad55ed966854d40b646ed36
Erkhes11 <93850961+Erkhes11@users.noreply.github.com>
2021-11-22 18:07:21 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+ScrollReveal().reveal('#zuun', {
+    origin: 'left',
+    distance: '100%'
+})
+
+ScrollReveal().reveal('#right', {
+    origin: 'right',
+    distance: '40px'
+})
+
+ScrollReveal().reveal('.hero', {
+    origin: 'left',
+    distance: '100%',
+    duration: 1500
+})
+
+ScrollReveal().reveal('#overlay', {
+    origin: 'left',
+    distance: '100%',
+    duration: 1200,
+    delay: 700
+})
